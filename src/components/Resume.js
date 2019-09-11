6b7505c9aa4bf30070ce7366376fef7feeb231cc
danielbrog <brog.daniel@gmail.com>
2019-09-11 12:33:43 -0400

    updated resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,9 +5,6 @@ const Resume = () => (
   <div>
     <Header />
     <div className="resume">
-      <a href="path_to_file" download="./documents/resume.pdf">
-        <button className="resume__button">Download Resume</button>
-      </a>
       <embed src="./documents/resume.pdf" width="800px" height="1100px" />
     </div>
   </div>
